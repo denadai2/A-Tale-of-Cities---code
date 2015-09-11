e63f6adc573290bca6d5a5a055475a07298c4fe1
Marco <marco@Marcos-MacBook-Air.local>
2015-09-11 12:17:07 +0200

    Revised version of the plots

--- plot.py
+++ plot.py
@@ -1,83 +1,144 @@
-__author__ = 'marco'
+__author__ = 'Marco De Nadai'
 __license__ = "MIT"
 
 import pandas as pd
 import numpy as np
-from datetime import datetime
-from scipy import stats
 import matplotlib.pyplot as plt
 import seaborn as sns
+from math import sqrt
+import datetime
 
 
-# Import the dataset
 parse = lambda x: datetime.datetime.fromtimestamp(float(x)/1000)
-sliceSum = pd.read_csv('datasets/sms-call-internet-tn-2013-11-01.txt', delim_whitespace=True, names=['GridCell', 'datetime','countryCode', 'callin', 'callout', 'smsin', 'smsout' ,'internet'], encoding="utf-8-sig", parse_dates=['datetime'], date_parser=parse)
 
-# Set the index as timestamp
-sliceSum = sliceSum.set_index(['datetime'], drop=False)
-sliceSum.index = sliceSum.index.tz_localize('UTC').tz_convert('Europe/Rome')
+fig_width_pt = 345  # Get this from LaTeX using \showthe\columnwidth
+inches_per_pt = 1.0/72.27               # Convert pt to inches
+golden_mean = (sqrt(5)-1.0)/2.0         # Aesthetic ratio
+fig_width = fig_width_pt*inches_per_pt  # width in inches
+fig_height = fig_width*golden_mean       # height in inches
+fig_size = [fig_width, fig_height]
 
-# Group everything, ignoring the countrycode and the days
-sliceSum = sliceSum.groupby(['GridCell'], as_index=False).sum()
-del sliceSum['countryCode']
+sns.set_style("ticks")
+sns.set_context("paper")
 
-# Absolute
-sliceSum['day'] = sliceSum.index.weekday
-abs_df = sliceSum.groupby(['day', sliceSum.index.hour]).mean()
+# Import dataset - Milano
+sliceSum = pd.DataFrame({})
+for index in range(4,11):
+    sliceSum2 = pd.read_csv('nature/sms-call-internet-mi-2013-11-'+str(index).zfill(2) +'.txt', sep='\t', encoding="utf-8-sig", names=['CellID', 'datetime', 'countrycode', 'smsin', 'smsout', 'callin', 'callout', 'internet'], parse_dates=['datetime'], date_parser=parse)
+    sliceSum2 = sliceSum2.set_index('datetime')
+    sliceSum2['hour'] = sliceSum2.index.hour
+    sliceSum2['weekday'] = sliceSum2.index.weekday
+    sliceSum2 = sliceSum2.groupby(['hour', 'weekday', 'CellID'], as_index=False).sum()
 
-# Z-scale
-weekly_df = sliceSum
-weekly_df = weekly_df.groupby(['day', sliceSum.index.hour]).mean()
-weekly_df = (weekly_df - weekly_df.mean()) / weekly_df.std()
+    sliceSum = sliceSum.append(sliceSum2)
 
-# Style managment
-sns.set_palette(sns.color_palette("Blues_r"))
-sns.set_style("white")
+sliceSum['idx'] = sliceSum['hour'] + (sliceSum['weekday']*24)
+sliceSum.head()
 
-types = ['smsin', 'smsout', 'callsin', 'callsout', 'internet']
+# Import dataset Tweets Milano
+social_df = pd.read_csv('nature/result2.csv', sep=',', encoding="utf-8-sig", parse_dates=['created'])
+social_df = social_df.set_index(['created'], drop=False)
+social_df = social_df.groupby([social_df.index.weekday, social_df.index.hour], as_index=False).count()
+social_df['Tweets'] = social_df['created']
+social_df = social_df[['Tweets']]
+social_df.head()
 
-# Plot of mean absolute values
-lines = []
-f, axs = plt.subplots(len(types), sharex=True, sharey=True,figsize=(8,11))
-for i,p in enumerate(types):
+# Group by weekday-hour
+sliceSum_city = sliceSum.groupby(['weekday', 'hour'], as_index=False).sum()
+sliceSum_city['sms'] = sliceSum_city['smsin'] + sliceSum_city['smsout']
+sliceSum_city['calls'] = sliceSum_city['callin'] + sliceSum_city['callout']
+sliceSum_city.rename(columns={'sms': 'SMS', 'internet': 'Internet'}, inplace=True)
 
-    plt.xticks(np.arange(168, step=8))
-    axs[i].plot(abs_df[p], label=p)
-    axs[i].legend()
+# Behaviour plot
+types = ['SMS', 'calls', 'Internet', 'Tweets']
+fig_size2 = [fig_width, fig_width*golden_mean*2 ]
+f, axs = plt.subplots(len(types), sharex=True, sharey=True,figsize=fig_size2)
+
+# Z-score
+sliceSum_z = (sliceSum_city - sliceSum_city.mean()) / sliceSum_city.std()
+social_df = (social_df - social_df.mean()) / social_df.std()
+
+for i, p in enumerate(types):
+    plt.xticks(np.arange(168, step=10))
+    if p == 'Tweets':
+        axs[i].plot(social_df[p], label=p)
+    else:
+        axs[i].plot(sliceSum_z[p], label=p)
+    axs[i].legend(loc='upper center')
     sns.despine()
 
-f.text(0.075, 0.5, "Number of events", rotation="vertical", va="center")
-
-plt.xlabel("Hour (in a week)")
-plt.savefig('absbehaviour.pdf', format='pdf', dpi=330,bbox_inches='tight')
-
-# Plot of mean Z-scaled values
-lines = []
-f, axs = plt.subplots(len(types), sharex=True, sharey=True,figsize=(8,11))
-for i,p in enumerate(types):
-
-    plt.xticks(np.arange(168, step=8))
-    axs[i].plot(weekly_df[p], label=p)
-    axs[i].legend()
-    sns.despine()
-
-f.text(0.075, 0.5, "Number of events", rotation="vertical", va="center")
+f.text(0, 0.5, "Number of events", rotation="vertical", va="center")
 
 plt.xlabel("Hour (in a week)")
 plt.savefig('behaviour.pdf', format='pdf', dpi=330,bbox_inches='tight')
 
-#
-# Boxplots
-#
-we_df = sliceSum.groupby(['day','gridcell']).mean()
-types_lim = [70, 70, 60, 60, 900]
+# Import dataset Telecommunications - Trentino
+sliceSum3 = pd.DataFrame({})
+for index in range(4, 11):
+    sliceSum2 = pd.read_csv('nature/sms-call-internet-tn-2013-11-'+str(index).zfill(2) +'.txt', sep='\t', encoding="utf-8-sig", names=['CellID', 'datetime', 'countrycode', 'smsin', 'smsout', 'callin', 'callout', 'internet'], parse_dates=['datetime'], date_parser=parse)
+    sliceSum2 = sliceSum2.set_index('datetime')
+    sliceSum2['hour'] = sliceSum2.index.hour
+    sliceSum2['weekday'] = sliceSum2.index.weekday
+    sliceSum2 = sliceSum2.groupby(['hour', 'weekday', 'CellID'], as_index=False).sum()
 
-for i,p in enumerate(types):
-    ax = we_df.reset_index().boxplot(column=p,by='day', grid=False)
-    ax.set_ylim([0, types_lim[i]])
-    plt.title("Average number of "+p+" events per day and grid square")
-    plt.suptitle("")
-    plt.ylabel("Number of events")
-    plt.xlabel("Weekday (0=Monday, 6=Sunday)")
+    sliceSum3 = sliceSum3.append(sliceSum2)
+
+sliceSum3['idx'] = sliceSum3['hour'] + (sliceSum3['weekday']*24)
+sliceSum3.head()
+
+# Validation behavioural plots
+fig_size2 = [fig_width,fig_width*golden_mean*1.5 ]
+fig = plt.figure(figsize=fig_size2)
+
+f, axs = plt.subplots(2, sharex=True, sharey=False, figsize=fig_size2)
+
+axs[0].plot(sliceSum[sliceSum.CellID == 5060].set_index('idx')['internet'], label='Duomo')
+axs[0].plot(sliceSum[sliceSum.CellID == 4259].set_index('idx')['internet'], label='Bocconi')
+axs[0].plot(sliceSum[sliceSum.CellID == 4456].set_index('idx')['internet'], label='Navigli')
+axs[0].set_xticklabels([])
+sns.despine()
+# Shrink current axis's height by 10% on the bottom
+box = axs[0].get_position()
+axs[0].set_position([box.x0, box.y0 + box.height * 0.1, box.width, box.height * 0.9])
+axs[0].legend(loc='upper center', bbox_to_anchor=(0.5, -0.10), fancybox=True, shadow=True, ncol=5)
+axs[1].plot(sliceSum3[sliceSum3.CellID == 5200].set_index('idx')['internet'], label='Duomo')
+axs[1].plot(sliceSum3[sliceSum3.CellID == 5085].set_index('idx')['internet'], label='Mesiano')
+axs[1].plot(sliceSum3[sliceSum3.CellID == 4703].set_index('idx')['internet'], label='Bosco')
+# Shrink current axis's height by 10% on the bottom
+box = axs[1].get_position()
+axs[1].set_position([box.x0, box.y0 + box.height * 0.1, box.width, box.height * 0.9])
+axs[1].legend(loc='upper center', bbox_to_anchor=(0.5, -0.30), fancybox=True, shadow=True, ncol=5)
+axs[1].set_xlabel("Weekly hour")
+sns.despine()
+
+f.text(0, 0.5, "Absolute number of connections", rotation="vertical", va="center")
+plt.savefig('validation-Milano.pdf', format='pdf', dpi=330, bbox_inches='tight')
+
+# Boxplots
+boxplots = {
+    'calls': "Calls",
+    'sms': "SMS",
+    "internet": "Internet CDRs"
+}
+fig_size2 = [fig_width, fig_width*golden_mean*2]
+f, axs = plt.subplots(len(boxplots.keys()), sharex=True, sharey=False, figsize=fig_size2)
+f.subplots_adjust(hspace=.35,wspace=0.1)
+sliceSum['sms'] = sliceSum['smsin'] + sliceSum['smsout']
+sliceSum['calls'] = sliceSum['callin'] + sliceSum['callout']
+i = 0
+plt.suptitle("")
+for k, v in boxplots.iteritems():
+
+    ax = sliceSum.reset_index().boxplot(column=k, by='weekday', grid=False, sym='', ax =axs[i])
+    axs[i].set_title(v)
+    axs[i].set_xlabel("")
     sns.despine()
-    plt.savefig('boxplot-'+p+'.pdf', format='pdf', dpi=330,bbox_inches='tight')
+    i += 1
+
+plt.xlabel("Weekday (0=Monday, 6=Sunday)")
+f.text(0, 0.5, "Number of events", rotation="vertical", va="center")
+plt.savefig('boxplots-Milano.pdf', format='pdf', dpi=330,bbox_inches='tight')
+
+
+
+
